e8261c29fcb44deabdc395390945195e0f287ff0
azizmd313 <azizmd313@gmail.com>
2018-07-21 10:13:25 +0600

    Variable Tutorial has been added

---
+++ BasicSyntax/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<!-- Commmenting in html -->
+
+<h1>My first PHP page</h1>
+
+<?php
+    echo "<h2>Hello World!</h2>";
+    echo "My very first page";
+
+    //This is a single line comment.
+    /*
+        This
+        is 
+        a 
+        multiline
+        comment.
+    */
+    echo "<br>";
+    echo '<a href="variables.php">Intro. to Variable</a>';
+?>
+
+</body>
+</html>
---
+++ BasicSyntax/variables.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    $txt = "DBMS Lab!";
+    echo $txt;
+    echo "<br>";
+    $x = 5;
+    $y = 10.5;
+    echo $x;
+    echo "<br>";
+    echo $y;
+    echo "<br>";
+    $txt = 3;
+    //Same output for both formats of echo.
+    echo  "<h3>This is header no.".$txt."</h3>";
+    echo  "<h3>This is header no. $txt </h3>";
+?>
+    <a href=""></a>
+</body>
+</html>
